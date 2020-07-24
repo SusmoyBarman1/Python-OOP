fcdfc25768e10de101e4802353f3d208ea78711b
SusmoyBarman1 <susmoy.barman.bd@gmail.com>
2020-07-24 20:50:20 +0600

    update class added

---
+++ 3. updateData.py
@@ -0,0 +1,30 @@
+class Human():
+    
+    def __init__(self):
+        self.name = 'susmoy'
+        self.age = 24
+
+    def updateName(self, name):
+        self.name = name
+
+    def updateAge(self, age):
+        self.age = age
+
+man1 = Human()
+man2 = Human()
+
+print(f'\nName of 1st person: {man1.name}')
+print(f'Age of 1st person: {man1.age}')
+print(f'Name of 2nd person: {man2.name}')
+print(f'Age of 2nd person: {man2.age}\n')
+
+print('\nUpdating the name of 2nd person\n')
+
+man2.updateName('Nipa')
+man2.updateAge(22)
+
+
+print(f'\nName of 1st person: {man1.name}')
+print(f'Age of 1st person: {man1.age}')
+print(f'Name of 2nd person: {man2.name}')
+print(f'Age of 2nd person: {man2.age}\n')
